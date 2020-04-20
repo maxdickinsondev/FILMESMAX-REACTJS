bd7c968df1ad206c2ba4d16ad4a85cce4032006d
Max Dickinson <maxsantistadickinson@gmail.com>
2020-04-20 00:52:31 -0300

    Stylization of film details.

--- src/pages/Details/index.js
+++ src/pages/Details/index.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
+import { FaRegMoneyBillAlt, FaRegClock, FaRegCalendarAlt } from 'react-icons/fa';
 
 import { Container, MovieImage, MovieInfo, Title, Description, 
-    Genres, OthersInfo, ReleaseData, Duration, Budget 
+    Info, Genres, OthersInfo, ReleaseData, Duration, Budget 
 } from './styles';
 
 export default class Details extends Component {
@@ -16,20 +17,43 @@ export default class Details extends Component {
     render() {
         return (
             <Container>
-                <MovieImage src="http://image.tmdb.org/t/p/w300/iBvo3qOPcmhlqAaJcXcQHtx2qLk.jpg"/>
+                <MovieImage 
+                    src="http://image.tmdb.org/t/p/w300/iBvo3qOPcmhlqAaJcXcQHtx2qLk.jpg"
+                />
     
                 <MovieInfo>
-                    <Title>Mortal Kombat Legends: Scorpion's Revenge</Title>
-                    <Description>
-                        After the vicious slaughter of his family by stone-cold mercenary Sub-Zero, Hanzo
-                        Hasashi is exiled to the torturous Netherrealm.
-                    </Description>
-                    <Genres> Fantasy, Action, Adventure, Animation </Genres>
+                    <Info>
+                        <Title>Mortal Kombat Legends: Scorpion's Revenge</Title>
+                        <Description>
+                            um texto qualquer so pra ver se não estar ultrapassando um texto qualquer so pra ver se não estar ultrapassando um texto qualquer so pra ver se não estar ultrapassando um texto qualquer so pra ver se não estar ultrapassando um texto qualquer so pra ver se não estar ultrapassando um texto qualquer so pra ver se não estar ultrapassando
+                        </Description>
+                        <Genres> Genres: Fantasy, Action, Adventure, Animation </Genres>
+                    </Info>
     
                     <OthersInfo>
-                        <ReleaseData> Release data: 11.4.2020 </ReleaseData>
-                        <Duration> Duration: 1h 20m </Duration>
-                        <Budget> Budget: $0 </Budget>
+                        <ReleaseData>
+                            <FaRegCalendarAlt 
+                                size={30}
+                                style={{ marginRight: '10px' }}
+                            />
+                            Release data: 11.4.2020 
+                        </ReleaseData>
+
+                        <Duration>
+                            <FaRegClock 
+                                size={30}
+                                style={{ marginRight: '10px' }} 
+                            />
+                            Duration: 1h 20m 
+                        </Duration>
+
+                        <Budget> 
+                            <FaRegMoneyBillAlt 
+                                size={30} 
+                                style={{ marginRight: '10px' }} 
+                            />
+                            Budget: $85.000.000 
+                        </Budget>
                     </OthersInfo>
                 </MovieInfo>
             </Container>
--- src/pages/Details/styles.js
+++ src/pages/Details/styles.js
@@ -3,56 +3,79 @@ import styled from 'styled-components';
 export const Container = styled.div`
     display: flex;
     flex-direction: row;
+    margin-right: 10px;
+    margin-left: 10px;
 `;
 
 export const MovieImage = styled.img`
-
+    width: 280px;
 `;
 
 export const MovieInfo = styled.div`
     max-width: 850px;
+    max-height: 362px;
+    max-width: 800px;
+    min-width: 800px;
     background: rgba(0, 0, 0, 0.5);
+`;
+
+export const Info = styled.div`
+    max-height: 300px;
+    min-height: 300px;
+    display: flex;
+    flex-direction: column;
 `;
 
 export const Title = styled.h1`
     margin-top: 20px;
     margin-bottom: 40px;
-    margin-left: 15px;
+    margin-left: 20px;
     color: #ffff00;
 `;
 
 export const Description = styled.span`
     margin-left: 20px;
+    padding-right: 10px;
+    max-height: 300px;
+    min-height: 190px;
     font-size: 20px;
     color: #ddd;
 `;
 
 export const Genres = styled.span`
     color: #fff;
+    margin-left: 20px;
 `;
 
 export const OthersInfo = styled.footer`
     display: flex;
     flex-direction: row;
+    align-items: center;
     justify-content: space-between;
     background: #000;
     padding: 15px;
-    margin-top: 259px;
+    margin-top: 62px;
 `;
 
 export const ReleaseData = styled.span`
     color: #fff;
     margin-left: 10px;
     font-size: 18px;
+    display: flex;
+    align-items: center;
 `;
 
 export const Duration = styled.span`
     color: #fff;
     font-size: 18px;
+    display: flex;
+    align-items: center;
 `;
 
 export const Budget = styled.span`
     color: #fff;
     font-size: 18px;
     margin-right: 10px;
+    display: flex;
+    align-items: center;
 `;
